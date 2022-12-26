fa1fb951cfa68a080d2dbb9746e3736c7dd11221
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-12-25 22:57:29 -0600

    My_first_bot

---
+++ exo_bot.py
@@ -0,0 +1,23 @@
+import requests
+s=0
+while True:
+    Token='5883620989:AAEt7JjUHu3sN3bBIAkgR1GR0IBo6CvgpiM'
+    url=f'https://api.telegram.org/bot{Token}/getUpdates'
+    response=requests.get(url=url)
+    response=response.json()['result']
+    n=len(response)
+    while n>s:
+        s=n
+        i=response[-1]
+        url1=f'https://api.telegram.org/bot{Token}/sendMessage'
+        chat_id=i['message']['chat']['id']
+        text=i['message']['text']
+        reply=i['message']['message_id']
+        pyload={
+            'chat_id':chat_id,
+            'text':text,
+            'reply_to_message_id':reply
+        }
+        j=requests.get(url=url1,params=pyload)
+
+
